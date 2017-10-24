d25938e67e12b293ddd602ea7d3e943a0dcd965d
mitul jindal <mituljindal@gmail.com>
2017-10-24 15:54:20 +0530

    first view setup

---
+++ Virtual-Tourist/MapViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Virtual-Tourist
+//
+//  Created by mitul jindal on 24/10/17.
+//  Copyright © 2017 mitul jindal. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func editPressed(_ sender: Any) {
+    }
+}
+
